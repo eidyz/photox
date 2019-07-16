e3b51bee8da8edcbc6f36fa924c372d486b58df2
Adam Sedláček <eidyz818@gmail.com>
2019-07-16 21:10:11 +0200

    preset filters init

--- src/components/VMenu.vue
+++ src/components/VMenu.vue
@@ -2,11 +2,14 @@
 <template>
   <div class="v-menu p-2 fill-space">
     <a-row class="pb-4">
-      <a-col>
-        <a-icon type="arrow-left" />
+      <a-col :span="12">
+        <!-- <a-icon type="arrow-left" /> -->
+      </a-col>
+      <a-col :span="12" class="text-right">
+        <a-icon type="sliders" @click="togglePresets" />
       </a-col>
     </a-row>
-    <a-row>
+    <a-row v-if="presets">
       <a-col :span="24">
         Brightness
         <a-slider
@@ -35,16 +38,27 @@
         />
       </a-col>
     </a-row>
+    <a-row v-if="!presets">
+      <a-col v-for="(filter, index) in filterPresets" :key="index" :span="24">
+        <div class="filter-preset-wrapper" @click="setFilter(filter)">
+          <div class="filter-preset">{{filter.name}}</div>
+        </div>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
+import filters from '../filters';
+
 export default {
   data() {
     return {
       brightness: 50,
       contrast: 50,
       saturation: 50,
+      presets: false,
+      filterPresets: filters,
     };
   },
   methods: {
@@ -57,6 +71,14 @@ export default {
     changeSaturation() {
       this.$store.commit('SET_SATURATION', this.saturation);
     },
+    setFilter(filter) {
+      this.$store.commit('SET_BRIGHTNESS', filter.brightness);
+      this.$store.commit('SET_CONTRAST', filter.contrast);
+      this.$store.commit('SET_SATURATION', filter.saturation);
+    },
+    togglePresets() {
+      this.presets = !this.presets;
+    },
   },
 };
 </script>
@@ -66,5 +88,25 @@ export default {
   color: #f8f8f2;
   background-color: #282a36;
   box-shadow: 1px 0px 4px rgba(0, 0, 0, 0.25);
+}
+
+.filter-preset-wrapper {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-bottom: 100%;
+  margin: 10px 0;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  .filter-preset{
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
---
+++ src/filters/index.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: 'Monday',
+    brightness: 60,
+    contrast: 50,
+    saturation: 60,
+  },
+  {
+    name: 'Sunlight',
+    brightness: 60,
+    contrast: 65,
+    saturation: 53,
+  },
+  {
+    name: 'Noir',
+    brightness: 40,
+    contrast: 62,
+    saturation: 0,
+  },
+  {
+    name: 'Pangea',
+    brightness: 40,
+    contrast: 62,
+    saturation: 18,
+  },
+];
